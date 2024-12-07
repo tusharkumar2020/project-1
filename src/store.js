85da473220c7bf208a2c21b13631e104e3ad5712
cristian <cristian.triboi@gmail.com>
2024-12-07 14:21:52 -0500

    Create the items and logic for the venue ad-ons

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
+import avSlice from './avSlice';
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avSlice,
   },
 });
